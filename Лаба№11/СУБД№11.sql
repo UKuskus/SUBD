f41ec673d8a09ca148759a614194d1c5c5d0102f
UKuskus <65870521+UKuskus@users.noreply.github.com>
2020-12-22 14:46:47 +0200

    Розробка та застосування транзакцій

---
+++ Лаба№11/СУБД№11.sql
@@ -0,0 +1,6 @@
+BEGIN TRANSACTION 
+Insert Into [Clother_Shop_Catalog].[dbo].[Дизайнер товару]
+Values 	(2, N'Шанель',N'Коко',N'NULL', '1998-09-15',6,N'Франція'),
+(4, N'Хміль',N'Василина',N'Романівна', '1991-11-25',8,N'Україна');
+COMMIT; 
+
